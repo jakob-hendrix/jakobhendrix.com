5983803e04a25754235c4d643bd32fb241f00411
p00293412 <jakob.hendrix@la.gov>
2019-03-12 15:47:29 -0500

    Cleaned up for the end of the chapter, and added a file to play around with file server stuff.

--- src/JakobHendrixCore/JakobHendrixCore/Startup.cs
+++ src/JakobHendrixCore/JakobHendrixCore/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace JakobHendrixCore
@@ -28,15 +30,15 @@ namespace JakobHendrixCore
             loggerFactory.AddConsole(LogLevel.Information);
             loggerFactory.AddDebug();
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("error.html");
-            //}
-            app.UseExceptionHandler("/Home/Error");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
 
             // Enable proper URI redirection when using a reverse-proxy
             app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -44,16 +46,12 @@ namespace JakobHendrixCore
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
-            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id ?}"
                 );
-            });
-            app.Run(context => {
-                throw new Exception("Not implemented yet");
             });
         }
     }
